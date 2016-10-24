0d08056942437f78794bd1bd564abed30edd16a1
Massimo Frascati <mfrascati@gmail.com>
2016-10-24 12:24:35 +0200

    Implementa CorsTrait per la gestione della risposta preflight

---
+++ src/Lib/CorsTrait.php
@@ -0,0 +1,27 @@
+<?php
+namespace Entheos\Utils\Lib;
+
+/**
+ * Trait for simplyfing handle of CORS headers and preflight response
+ */
+trait CorsTrait {
+
+	public function setCorsHeaders()
+	{
+		// Allow from any origin
+		if (isset($_SERVER['HTTP_ORIGIN'])) {
+		    header("Access-Control-Allow-Origin: *");
+		    header('Access-Control-Allow-Credentials: true');
+		    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+		}
+		// Access-Control headers are received during OPTIONS requests
+		if (!empty($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+		    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+		        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");         
+
+		    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+		        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+		}
+	}
+}
