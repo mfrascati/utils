7bc4cf17e800f7aacaef94ae5f74fd8ceae90fe8
Massimo Frascati <mfrascati@gmail.com>
2016-12-21 18:55:36 +0100

    Mostra errori di validazione nella risposta json

--- src/Error/ApiExceptionRenderer.php
+++ src/Error/ApiExceptionRenderer.php
@@ -10,11 +10,15 @@ class ApiExceptionRenderer extends ExceptionRenderer
 		if(!$this->controller->request->is('json'))
 			return parent::_outputMessage($template);
 		
-	    $this->controller->set('success', false);
-	    $this->controller->set('data', [
+		$data = [
 	        'error' => $this->controller->viewVars['message'],
 	        'code' => $this->controller->viewVars['code']
-	    ]);
+	    ];
+	    if($data['code'] == 422 && !empty($this->controller->viewVars['error']))
+	    	$data['validation'] = $this->controller->viewVars['error']->getValidationErrors();
+
+	    $this->controller->set('success', false);
+	    $this->controller->set('data', $data);
 	    $this->controller->set('_serialize', ['success', 'data']);
 
 	    return parent::_outputMessage($template);
