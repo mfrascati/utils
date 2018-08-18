22dcde17336d2cf1983c1f3763f5c424da5d88b8
Massimo Frascati <mfrascati@gmail.com>
2018-08-18 17:29:39 +0200

    Limita CORS al dominio dell'app

--- src/Lib/CorsTrait.php
+++ src/Lib/CorsTrait.php
@@ -11,7 +11,7 @@ trait CorsTrait {
 	{
 		// Allow from any origin
 		if (isset($_SERVER['HTTP_ORIGIN'])) {
-		    header("Access-Control-Allow-Origin: *");
+		    header("Access-Control-Allow-Origin: " . \Cake\Routing\Router::fullBaseUrl());
 		    header('Access-Control-Allow-Credentials: true');
 		    header('Access-Control-Max-Age: 86400');    // cache for 1 day
 		}
