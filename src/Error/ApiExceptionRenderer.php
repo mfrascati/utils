4172e10aa7ae9a5d936b0d80abe692d28a47c5f2
mfrascati <mfrascati@gmail.com>
2022-02-12 19:23:22 +0100

    Updates api exception renderer

--- src/Error/ApiExceptionRenderer.php
+++ src/Error/ApiExceptionRenderer.php
@@ -2,29 +2,32 @@
 namespace Entheos\Utils\Error;
 
 use Cake\Error\ExceptionRenderer;
+use Cake\Http\Response;
 use Cake\Collection\Collection;
 
 class ApiExceptionRenderer extends ExceptionRenderer
 {
-	protected function _outputMessage($template)
+	protected function _outputMessage(string $template): Response
 	{
-		if(!$this->controller->request->is('json'))
+		$vars = $this->controller->viewBuilder()->getVars();
+		logd($vars);
+		if(!$this->controller->getRequest()->is('json'))
 		{	
 			if(\Cake\Core\Configure::read('debug'))
 				return parent::_outputMessage($template);
 			else
-				die('<div style="text-align:center; margin-top:40px"><h3>'.$this->controller->viewVars['message'].'</h3><h1>'.$this->controller->viewVars['error']->getMessage().'</h1></div>');
+				die('<div style="text-align:center; margin-top:40px"><h3>'.$vars['message'].'</h3><h1>'.$vars['error']->getMessage().'</h1></div>');
 		}
 		
 		$data = [
-	        'error' => $this->controller->viewVars['message'],
-	        'code' => $this->controller->viewVars['code']
+	        'error' => $vars['message'],
+	        'code' => $vars['code']
 	    ];
-	    if($data['code'] == 422 && !empty($this->controller->viewVars['error'])){
+	    if($data['code'] == 422 && !empty($vars['error'])){
 	    	$data['error']		= 'Sono presenti degli errori nei dati inviati:';
 	    	$validationErrors = [];
 
-	    	foreach($this->controller->viewVars['error']->getValidationErrors() as $field => $errors)
+	    	foreach($vars['error']->getValidationErrors() as $field => $errors)
 	    	{
 	    		foreach($errors as $rule => $message)
 	    			$validationErrors[] = ['field' => $field, 'rule' => $rule, 'message' => $message];
@@ -43,7 +46,7 @@ class ApiExceptionRenderer extends ExceptionRenderer
 
 	    $this->controller->set('success', false);
 	    $this->controller->set('data', $data);
-	    $this->controller->set('_serialize', ['success', 'data']);
+			$this->controller->viewBuilder()->setOption('serialize', ['success', 'data']);
 
 	    return parent::_outputMessage($template);
 	}
